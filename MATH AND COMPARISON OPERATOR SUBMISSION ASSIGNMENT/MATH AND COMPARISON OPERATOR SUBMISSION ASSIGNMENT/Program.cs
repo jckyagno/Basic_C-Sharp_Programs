605ee85f9f5fc43e0fd7173785becf77697ad681
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-08-28 10:40:35 -1000

    Finished comments

--- MATH AND COMPARISON OPERATOR SUBMISSION ASSIGNMENT/MATH AND COMPARISON OPERATOR SUBMISSION ASSIGNMENT/Program.cs
+++ MATH AND COMPARISON OPERATOR SUBMISSION ASSIGNMENT/MATH AND COMPARISON OPERATOR SUBMISSION ASSIGNMENT/Program.cs
@@ -11,13 +11,18 @@ namespace MATH_AND_COMPARISON_OPERATOR_SUBMISSION_ASSIGNMENT
         static void Main(string[] args)
         {
             Console.WriteLine("Anonymous Income Comparison Program\n\nPerson 1:\nPlease input Hourly Rate:");
+            //store input as input 1
             string input1 = Console.ReadLine();
+            //converts input1 to int
             int person1HR = Convert.ToInt32(input1);
             Console.WriteLine("\nPlease input hours worked per week:");
+            //same as above
             string input2 = Console.ReadLine();
             int person1HW = Convert.ToInt32(input2);
+            //summary of inputs to console
             Console.WriteLine("\nPerson 1:\nHourly Rate: \t\t\t\t" + person1HR + "\nHours worked per week: \t\t\t" + person1HW);
 
+            //similar to person 1
             Console.WriteLine("\nPerson 2:\nPlease input Hourly Rate:");
             string input3 = Console.ReadLine();
             int person2HR = Convert.ToInt32(input3);
@@ -26,13 +31,17 @@ namespace MATH_AND_COMPARISON_OPERATOR_SUBMISSION_ASSIGNMENT
             int person2HW = Convert.ToInt32(input4);
             Console.WriteLine("\nPerson 2:\nHourly Rate: \t\t\t\t" + person2HR + "\nHours worked per week: \t\t\t" + person2HW);
 
+            //int to combine hourly wage, hours worked per week, and 52 weeks in a year
             int annualSalary1 = person1HR * person1HW * 52;
             Console.WriteLine("\nAnnual Salary of Person 1: \t\t" + annualSalary1);
+            //same as above
             int annualSalary2 = person2HR * person2HW * 52;
             Console.WriteLine("Annual Salary of Person 2: \t\t" + annualSalary2);
 
             Console.WriteLine("\nDoes Person 1 make more money than Person 2?");
+            //bool to compare products of salaries
             bool comparison = annualSalary1 > annualSalary2;
+            //displays results
             Console.WriteLine(comparison);
             Console.ReadLine();
         }
