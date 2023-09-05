1d5f7b4e51417dcfd6bc800d5d88bed7f664929d
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-09-05 12:45:24 -1000

    Completed CALLING METHODS SUBMISSION ASSIGNMENT

--- CLASSES AND OBJECTS/CALLING METHODS SUBMISSION ASSIGNMENT/CALLING METHODS SUBMISSION ASSIGNMENT/Program.cs
+++ CLASSES AND OBJECTS/CALLING METHODS SUBMISSION ASSIGNMENT/CALLING METHODS SUBMISSION ASSIGNMENT/Program.cs
@@ -10,20 +10,17 @@ namespace CALLING_METHODS_SUBMISSION_ASSIGNMENT
     {
         static void Main(string[] args)
         {
-            //Calculation calculation = new Calculation();
-            //calculation = MethodOne(calculation);
-            //int input = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine(calculation);
-            //Console.ReadLine();
 
             Console.WriteLine("Please enter a number:");
+            //asks the user for input to multiply with
             int input = Convert.ToInt32(Console.ReadLine());
 
-
+            //uses input and grabs each return and stores in answers
             int answerOne = Calculation.MethodOne(input);
             int answerTwo = Calculation.MethodTwo(input);
             int answerThree = Calculation.MethodThree(input);
+
+            //displays answers to user
             Console.WriteLine(input + " times 5 is: " + answerOne);
             Console.WriteLine(input + " times 10 is: " + answerTwo);
             Console.WriteLine(input + " times 15 is: " + answerThree);
