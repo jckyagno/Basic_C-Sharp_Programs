9b5f8a7f961e6095d28840e813f7f620e84658a4
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-09-05 09:22:43 -1000

    Completed CONSOLE APP STRINGS AND INTEGERS SUBMISSION ASSIGNMENT

---
+++ EXCEPTION HANDLING/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //try
+        //{
+        //    Console.WriteLine("Pick a number.");
+        //    int numberOne = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Pick a second number.");
+        //    int numberTwo = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Dividing the two...");
+        //    int numberThree = numberOne / numberTwo;
+        //    Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+        //    Console.ReadLine();
+        //}
+        //catch (FormatException ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //    return;
+        //}
+        //catch (DivideByZeroException ex)
+        //{
+        //    Console.WriteLine("Please don't divide by zero.");
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
+        //finally
+        //{
+        //    Console.ReadLine();
+        //}
+
+
+        List<int> numbers = new List<int>() { 420, 840, 1680 };
+        Console.WriteLine("Type a number to divide by:");
+
+
+
+        try
+        {
+            int divNum = Convert.ToInt32(Console.ReadLine());
+            foreach (int number in numbers)
+            {
+
+                int quotient = number / divNum;
+                Console.WriteLine(divNum + " divided by " + number + " is: " + quotient);
+            }
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Ending program...");
+            Console.ReadLine();
+        }
+
+
+    }
+}
