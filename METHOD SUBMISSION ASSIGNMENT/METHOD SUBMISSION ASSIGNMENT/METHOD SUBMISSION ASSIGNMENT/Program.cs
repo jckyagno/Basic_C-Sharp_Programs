e03cc3874cfad42a3e8d787bdc610aedaba5c852
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-09-06 07:43:45 -1000

    Corrected METHOD SUBMISSION ASSIGNMENT

--- METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -16,18 +16,26 @@ namespace METHOD_SUBMISSION_ASSIGNMENT
             //asks user for first number (mandatory)
             Console.WriteLine("Please input a number:");
             //stores first number as an int to be used
-            int inputOne = Convert.ToInt32(Console.ReadLine());
+            int input1 = Convert.ToInt32(Console.ReadLine());
 
             //asks user for second number (optional)
             Console.WriteLine("Please input another number (or leave blank):");
             //stores second number as string, necessary for the optional functionality
             string inputTwo = Console.ReadLine();
 
+            if (inputTwo == "")
+            {
+                string response = input1 + " * 1 = " + calculation.Method(input1);
+                Console.WriteLine(response);
+            }
+            else
+            {
+                int input2 = Convert.ToInt32(inputTwo);
+                string response = input1 + " * " + input2 + " = " + calculation.Method(input1, input2);
+                Console.WriteLine(response);
+            }
 
-            //create response string from returns
-            string response = calculation.Method(inputOne, inputTwo);
-            //display response
-            Console.WriteLine(response);
+
             Console.ReadLine();
         }
     }
