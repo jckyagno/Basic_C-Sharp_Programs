169bcf4ecb17f3349728d5d14c7462b4ddaecd6c
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-09-05 15:26:30 -1000

    Commented assignment

--- METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/Calculation.cs
+++ METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/Calculation.cs
@@ -8,11 +8,15 @@ namespace METHOD_CLASS_SUBMISSION_ASSIGNMENT
 {
     class Calculation
     {
+        //void method, no return
         public void Method(int a, int b)
         {
+            //input a used in math operation
             int answer = a + 2;
 
+            //sum of math operation displayed
             Console.WriteLine("\n" + a + " plus 2 is " + answer + ".");
+            //input 2 displayed by itself.
             Console.WriteLine(b + " is by itself.");
         }
     }
--- METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/Program.cs
+++ METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/Program.cs
@@ -13,12 +13,15 @@ namespace METHOD_CLASS_SUBMISSION_ASSIGNMENT
             //instatiate class
             Calculation calculation = new Calculation();
 
+            //asks for user input 1, stored as a
             Console.WriteLine("Please input a number to perform a math operation:");
             int a = Convert.ToInt32(Console.ReadLine());
 
+            //asks for user input 2, stored as b
             Console.WriteLine("\nPlease input a number to just show on the console:");
             int b = Convert.ToInt32(Console.ReadLine());
 
+            //run method
             calculation.Method(a, b);
             Console.ReadLine();
         }
