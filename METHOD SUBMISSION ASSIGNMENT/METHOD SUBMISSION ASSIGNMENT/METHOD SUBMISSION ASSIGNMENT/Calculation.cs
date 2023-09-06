b3918b5f97464ada0b9cde99c32eecf21f53a8ec
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-09-05 14:49:02 -1000

    Completed METHOD SUBMISSION ASSIGNMENT

---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Calculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_SUBMISSION_ASSIGNMENT
+{
+    class Calculation
+    {
+        //method intakes two inputs
+        public string Method(int inputOne, string inputTwo)
+        {
+            //if there is nothing typed for input two...
+            if (inputTwo == "")
+            {
+                //convert the "" to acutally zero
+                int answer = Convert.ToInt32(inputOne);
+                string response = "The number you entered is: " + answer;
+                //return response string to be used in main program
+                return response;
+            }
+            //if number was typed for second number...
+            else
+            {
+                //convert string to int
+                int inputTwoA = Convert.ToInt32(inputOne);
+                //perform calculation
+                int answer = inputOne + inputTwoA;
+                string response = inputOne + " plus " + inputTwoA + " equals " + answer;
+                //return response string to be used in main program
+                return response;
+            }
+        }
+        
+
+
+    }
+}
---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate class
+            Calculation calculation = new Calculation();
+
+            //asks user for first number (mandatory)
+            Console.WriteLine("Please input a number:");
+            //stores first number as an int to be used
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+
+            //asks user for second number (optional)
+            Console.WriteLine("Please input another number (or leave blank):");
+            //stores second number as string, necessary for the optional functionality
+            string inputTwo = Console.ReadLine();
+
+
+            //create response string from returns
+            string response = calculation.Method(inputOne, inputTwo);
+            //display response
+            Console.WriteLine(response);
+            Console.ReadLine();
+        }
+    }
+}
