2727df2c3af30d67dbe0be29ede50ac9b8aa97b2
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-09-07 09:35:59 -1000

    Completed OPERATORS SUBMISSION

---
+++ OPERATORS SUBMISSION/OPERATORS SUBMISSION/OPERATORS SUBMISSION/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATORS_SUBMISSION
+{
+    public class Employee
+    {
+        //created id, firstname, lastname
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        bool isEqual = false;
+
+        //overload == operator
+        public static string operator==(Employee employee1, Employee employee2)
+        {
+            //if employee ids are equal
+            if (employee1.Id == employee2.Id)
+            {
+                string response = "Employee IDs are equal. (==)";
+                return response;
+            }
+            else
+            {
+                string response = "Employee IDs are not equal. (==)";
+                return response;
+            }
+        }
+        //overloads != operator
+        public static string operator!=(Employee employee1, Employee employee2)
+        {
+            //if employee ids are not equal
+            if (employee1.Id != employee2.Id)
+            {
+                string response = "Employee IDs are not equal. (!=)";
+                return response;
+            }
+            else
+            {
+                string response = "Employee IDs are equal. (!=)";
+                return response;
+            }
+
+        }
+    }
+}
---
+++ OPERATORS SUBMISSION/OPERATORS SUBMISSION/OPERATORS SUBMISSION/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATORS_SUBMISSION
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //instantiates two employees
+            Employee employee1 = new Employee() { Id = 111, FirstName = "Sample", LastName = "Salaryman" };
+            Employee employee2 = new Employee() { Id = 222, FirstName = "Example", LastName = "Employee" };
+
+            //write responses to console
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 != employee2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
