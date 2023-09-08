3a0ff6aa0ec358ca230fffb8251aaf20ffcc8867
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-09-08 12:27:04 -1000

    Complete STRUCT ASSIGNMENT

---
+++ STRUCT/STRUCT/STRUCT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRUCT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of data type Number
+            Number number = new Number();
+            //assigns 0.07 to number.Amount
+            number.Amount = .07m;
+
+            //print to console number.Amount
+            Console.WriteLine(number.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
